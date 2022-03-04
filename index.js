3348622f4607549b427cc6bfd34df0eb5305b200
Harshit <harshitbargali567@gmail.com>
2022-03-04 23:25:22 +0530

    hodlinfo

--- index.js
+++ index.js
@@ -14,10 +14,38 @@ const Crypto = require('./model/model')
 
 const port = process.env.PORT || 3000
 
+const session = require('express-session')
+
+const MongoDBStore = require("connect-mongo");
+
 const dbURL = process.env.DB_URL || 'mongodb://localhost:27017/crypto'
 mongoose.connect(dbURL)
 .then(console.log('Connected to the database'))
 .catch(err=>console.log('Error'))
+
+const store = new MongoDBStore({
+    mongoUrl: dbURL,
+    secret: 'secretCode',
+    collectionName:'session',
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
+
+const sessionDetail = {
+    store,
+    secret: 'secretCode',
+    saveUninitialized: true,
+    resave:false,
+    cookie:{
+        httpOnly: true,
+        expires: Date.now() + 1000*60*60*24*7,
+        maxAge:1000*60*60*24*7
+    }
+}
+app.use(session(sessionDetail))
 
 app.engine('ejs', ejsMate)
 app.set('views', path.join(__dirname, 'views'))
@@ -27,7 +55,7 @@ app.use(express.static(path.join(__dirname, '/public')))
 app.use(express.urlencoded({extended:true}))
 app.get('/', async(req, res)=>{
     const {data} = await axios.get('https://api.wazirx.com/api/v2/tickers')
-await Crypto.deleteMany()
+    await Crypto.deleteMany()
     let c =0
     for(let key in data)
     {
